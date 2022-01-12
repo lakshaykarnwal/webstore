c0206feb4d3628d8aa1522b705c794db54965a04
lakshaykarnwal <lakshaykarnwal@gmail.com>
2022-01-11 19:12:37 -0800

    added files

---
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useFormContext, Controller } from 'react-hook-form';
+import { TextField, Grid } from '@material-ui/core';
+
+const FormInput= ({ name, label }) => {
+  const { control } = useFormContext();
+
+  return (
+    <Grid item xs={12} sm={6}>
+      <Controller name={name} control={control} render = {({field}) => (
+            <TextField label={label} fullWidth required defaultValue=""/>
+        )}
+      />
+    </Grid>
+  );
+}
+
+export default FormInput;
---
+++ src/lib/commerce.js
@@ -0,0 +1,3 @@
+import Commerce from  '@chec/commerce.js';
+
+export const commerce = new Commerce('pk_test_37762541d1d725edf4172d7a6591c25ca9d09e4694e1d');
